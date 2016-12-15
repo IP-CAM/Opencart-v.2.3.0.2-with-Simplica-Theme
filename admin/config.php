4a561b54b8955db865f89835c53f82b87da34cbc
SeizV <vitaliiseiz@gmail.com>
2016-12-15 20:08:28 +0200

    install opencart(default) in linux

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart_task.local/admin/');
+define('HTTP_CATALOG', 'http://opencart_task.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart_task.local/admin/');
+define('HTTPS_CATALOG', 'http://opencart_task.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/seiz/PhpstormProjects/openCart_task/admin/');
+define('DIR_SYSTEM', '/home/seiz/PhpstormProjects/openCart_task/system/');
+define('DIR_IMAGE', '/home/seiz/PhpstormProjects/openCart_task/image/');
+define('DIR_LANGUAGE', '/home/seiz/PhpstormProjects/openCart_task/admin/language/');
+define('DIR_TEMPLATE', '/home/seiz/PhpstormProjects/openCart_task/admin/view/template/');
+define('DIR_CONFIG', '/home/seiz/PhpstormProjects/openCart_task/system/config/');
+define('DIR_CACHE', '/home/seiz/PhpstormProjects/openCart_task/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/seiz/PhpstormProjects/openCart_task/system/storage/download/');
+define('DIR_LOGS', '/home/seiz/PhpstormProjects/openCart_task/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/seiz/PhpstormProjects/openCart_task/system/storage/modification/');
+define('DIR_UPLOAD', '/home/seiz/PhpstormProjects/openCart_task/system/storage/upload/');
+define('DIR_CATALOG', '/home/seiz/PhpstormProjects/openCart_task/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'seiz');
+define('DB_PASSWORD', 'rhtvtyxeu');
+define('DB_DATABASE', 'opencart_task');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://opencart_task.local/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://opencart_task.local/');
+
+// DIR
+define('DIR_APPLICATION', '/home/seiz/PhpstormProjects/openCart_task/catalog/');
+define('DIR_SYSTEM', '/home/seiz/PhpstormProjects/openCart_task/system/');
+define('DIR_IMAGE', '/home/seiz/PhpstormProjects/openCart_task/image/');
+define('DIR_LANGUAGE', '/home/seiz/PhpstormProjects/openCart_task/catalog/language/');
+define('DIR_TEMPLATE', '/home/seiz/PhpstormProjects/openCart_task/catalog/view/theme/');
+define('DIR_CONFIG', '/home/seiz/PhpstormProjects/openCart_task/system/config/');
+define('DIR_CACHE', '/home/seiz/PhpstormProjects/openCart_task/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/seiz/PhpstormProjects/openCart_task/system/storage/download/');
+define('DIR_LOGS', '/home/seiz/PhpstormProjects/openCart_task/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/seiz/PhpstormProjects/openCart_task/system/storage/modification/');
+define('DIR_UPLOAD', '/home/seiz/PhpstormProjects/openCart_task/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'seiz');
+define('DB_PASSWORD', 'rhtvtyxeu');
+define('DB_DATABASE', 'opencart_task');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
